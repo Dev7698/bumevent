351093ab9ab54b9e3ec07ff62ac8df7dbd908848
dev patel <dev patel@DESKTOP-WEBDEVL>
2025-02-26 09:45:40 +0530

    comit

---
+++ BmuEvent_API/Models/ApplicationDbContext .cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace bumevent.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+
+        public DbSet<Event> Events { get; set; }
+    }
+}
---
+++ BmuEvent_API/Models/Event.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace bumevent.Models
+{
+    public class Event
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string EventTitle { get; set; }
+
+        [Required]
+        public DateTime EventDate { get; set; }
+
+        [Required]
+        public string EventPlace { get; set; }
+
+        [Required]
+        public string CoordinatorName { get; set; }
+
+        [Required]
+        public int StudentCount { get; set; }
+
+        [Required]
+        public string DepartmentName { get; set; }
+
+        public string Objective { get; set; }
+
+        public string ImagePath { get; set; }
+    }
+}
